2f90a3f272998ddce383bae38591a6fbbe7f6af9
ViniFernandesDev <vinifernandesdeveloper@gmail.com>
2022-07-12 14:22:06 -0300

    Create Header and contentEmphasis

--- header.php
+++ header.php
@@ -10,13 +10,17 @@
         <div class="navigation">
             <nav>
                 <ul>
-                    <li><a href="index.php">Home</a></li> 
-                    <li><a href="sobre_nos.php">A Empresa</a></li> 
-                    <li><a href="produtos_servicos.php">Produtos e Serviços</a></li> 
-                    <li><a href="contato.php">Contato</a></li> 
+                    <li><a href="index.php">Quem somos</a></li>
+                    <li><a href="index.php">Solução LR Seg</a></li>
+                    <li><a href="index.php">Planos e Valores</a></li>
+                    <li><a href="index.php">Fale Conosco </a></li>
                 </ul>
             </nav>
         </div><!--navigation-->
+
+        <div class="button --type_1">
+            <a href="">SOLICITE UMA DEMOSTRAÇÃO</a>
+        </div><!--button-->
 
     </div><!--container-->
 </header>
--- index.php
+++ index.php
@@ -9,8 +9,68 @@
 
 <?php include('header.php'); ?>
 
-<section class="banner">
-	<img src="assets/img/banner.jpg" alt="Banner">
+<section class="banner" style="margin-top:200px;">
+     <img src="assets/img/banner.jpg" alt="Banner">
 </section><!-- banner -->
+
+<section class="contentEmphasis">
+     <div class="container">
+
+          <div class="__left">
+               <h1>PRATICIDADE <br> E AGILIDADE</h1>
+
+               <p>De fim ao uso de papel para coleta de dados, check lists, avaliações e outros mais. A solução <b>LR Seg</b> permite reunir todas essas informações de forma digital e prática em um único lugar e compartilhar com diversas outras ferramentas.</p>
+
+               <div class="__list">
+                    <div>
+                    <a href="">
+                         <figure><img src="assets/img/emphasis/icone1.png" alt=""></figure>
+                         <span>Agilidade na elaboração de contratos</span>
+                    </a>
+                    </div>
+
+                    <div>
+                    <a href="">
+                         <figure><img src="assets/img/emphasis/icone2.png" alt=""></figure>
+                         <span>Rapidez e qualidade na coleta de dados</span>
+                    </a>
+                    </div>
+
+                    <div>
+                    <a href="">
+                         <figure><img src="assets/img/emphasis/icone3.png" alt=""></figure>
+                         <span>Elaboração de protocolo de entrega dos documentos</span>
+                    </a>
+                    </div>
+
+                    <div>
+                    <a href="">
+                         <figure><img src="assets/img/emphasis/icone4.png" alt=""></figure>
+                         <span>Elaboração de recibo de valores devidos</span>
+                    </a>
+                    </div>
+
+                    <div>
+                    <a href="">
+                         <figure><img src="assets/img/emphasis/icone5.png" alt=""></figure>
+                         <span>Acesso rápido as informações</span>
+                    </a>
+                    </div>
+
+                    <div>
+                    <a href="">
+                         <figure><img src="assets/img/emphasis/icone6.png" alt=""></figure>
+                         <span>Compartilhamento com diversas ferramentas</span>
+                    </a>
+                    </div>
+               </div><!--__list-->
+          </div><!--__left-->
+
+          <div class="__right">
+               <img src="assets/img/lrseg_smartphone_diferenciais.png" alt="">
+          </div><!--__right-->
+
+     </div><!--container-->
+</section><!--contentEmphasis-->
 
 <?php include('footer.php'); ?>
