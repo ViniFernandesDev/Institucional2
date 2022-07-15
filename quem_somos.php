0879d7dc14c49584ad89c478d9d57ccd32d6850d
lucasWeirich <lucasweirich11@gmail.com>
2022-07-15 09:15:58 -0300

    Create page Quem Somos

--- quem_somos.php
+++ quem_somos.php
@@ -9,14 +9,57 @@
 
 <?php include('header.php'); ?>
 
-<main class="internal">
-     <div class="container">
+<main class="internal sobreNos">
 
-          <section class="__titleMain">
+          <section class="__titleMain container">
                <h1>QUEM SOMOS</h1>
           </section><!--__titleMain-->
 
-     </div><!--container-->
+          <section class="__subTitle container">
+               <h2>A LR <span>SEG</span></h2>
+          </section><!-- __subtitle -->
+
+          <section class="content_1">
+               <div class="container">
+                    <div class="display">
+                         <div class="txt">
+                              <p>A empresa LR Seg é uma empresa formada por profissionais de áreas multidisciplinares, criadora de um aplicativo utilizado por empresas de assessoria em medicina e segurança no trabalho</p>
+
+                              <h3>Nosso principal produto é um aplicativo que agiliza os trabalhos relacionados a captação de clientes, coleta de dados para a elaboração dos programas de medicina e segurança no trabalho, registro de entrega e faturamento.</h3>
+
+                              <p> O principal diferencial da empresa é que as informações lançadas dentro do aplicativo podem ser utilizadas em qualquer outro software gerador dos documentos de medicina e segurança no trabalho, reduzindo em até 50% do tempo necessário para a elaboração dos programas de medicina e segurança no trabalho. Além da segurança e confiabilidade das informações a serem repassadas dos técnicos externos para a equipe interna desenvolvedora dos referidos documentos.</p>
+                         </div>
+                         <div class="img">
+                              <div class="imgMain"><img src="assets/img/sobrenos.jpg" alt=""></div>
+                              <span class="circle1"><img src="assets/img/circle_blue_big.png" alt=""></span>
+                              <span class="circle2"><img src="assets/img/circle_ball.png" alt=""></span>
+                              <span class="circle3"><img src="assets/img/circle_blue_big.png" alt=""></span>
+                              <span class="circle4"><img src="assets/img/circle_ball.png" alt=""></span>
+                              <span class="circle5"><img src="assets/img/circle_green_small.png" alt=""></span>
+                         </div>
+                    </div><!-- display -->
+               </div><!-- container -->
+          </section><!-- content-1 -->
+
+          <section class="content_2">
+               <div class="container">
+                    <div class="content">
+                         <div class="main">
+                              <h2>Organização <br> e Optimização</h2>
+                         </div><!-- main -->
+                         <div class="footer">
+                              <h2>Na palma da sua mão</h2>
+
+                              <div class="button">
+                                   <a href="#">FAÇA UMA AVALIAÇÃO GRATUITA</a>
+                              </div>
+                              <div class="imgMain"><img src="assets/img/organized_device.png" alt=""></div>
+                              <div class="imgSecond"><img src="assets/img/circle_blue_big.png" alt=""></div>
+                         </div><!-- footer -->
+                    </div><!-- content -->
+               </div><!-- container -->
+          </section><!-- content_2 -->
+
 </main><!--internal-->
 
 <?php include('footer.php'); ?>
